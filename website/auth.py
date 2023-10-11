5701a460727b2ce5ae57a414ca14f56bf453b78f
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-10-10 23:13:36 -0400

    removed incorrect request method processing in login, added request method processing to signup and added error messages to base.html

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,9 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    data = request.form
-    print(data)
     return render_template("login.html")
 
 @auth.route("/logout")
@@ -14,4 +12,22 @@ def logout():
 
 @auth.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        #a few basic python checks
+        if len(email) < 4:
+            flash("Email must be greater than 4 characters", category="error")
+        elif len(firstName) < 2:
+            flash("First name must be greater than 1 character", category="error")
+        elif password1 != password2:
+            flash("Passwords do not match", category="error")
+        elif len(password1) < 7:
+            flash("Passwords must be at least 7 categories", category="error")
+        else:
+            flash("Account created!", category="success")
+
     return render_template("signup.html")
