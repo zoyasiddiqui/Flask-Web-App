e8588bdc5136260da8f12a7dc237abbfbb00b8e9
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-10-10 23:46:32 -0400

    fix typos in models.py

--- website/models.py
+++ website/models.py
@@ -10,12 +10,12 @@ class Note(db.Model):
     #how we can associate a note with a user
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-class User(db.model, UserMixin):
+class User(db.Model, UserMixin):
     #defining layout for object in our database
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    first_name = db.Coumn(db.String(150))
+    first_name = db.Column(db.String(150))
 
     #link them to notes
     notes = db.relationship('Note') #can think of this like a list of note we can access by name
