598ef1e2d4c577dfdfaed7206366bb29b2887deb
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-10-10 23:23:13 -0400

    define models

---
+++ website/models.py
@@ -0,0 +1,21 @@
+from . import db 
+from flask_login import UserMixin
+from sqlalchemy import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    #how we can associate a note with a user
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.model, UserMixin):
+    #defining layout for object in our database
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Coumn(db.String(150))
+
+    #link them to notes
+    notes = db.relationship('Note') #can think of this like a list of note we can access by name
