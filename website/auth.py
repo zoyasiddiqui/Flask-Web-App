a80f73f5fe48fa18bb39018f899372a0b251e273
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-10-10 22:28:14 -0400

    setup routes in auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,15 @@
 from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
+
+@auth.route("/login")
+def login():
+    return ""
+
+@auth.route("/logout")
+def logout():
+    return ""
+
+@auth.route("/signup")
+def signup():
+    return ""
