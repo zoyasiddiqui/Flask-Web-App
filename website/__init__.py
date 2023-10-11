a35cab305680764278348374e1fd4fafc2a1fd47
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-10-10 22:26:22 -0400

    registered blueprints

--- website/__init__.py
+++ website/__init__.py
@@ -6,4 +6,9 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "random"
 
+    from .views import views
+    from .auth import auth
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
     return app
