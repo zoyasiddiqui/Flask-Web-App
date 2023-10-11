50c3ee332292cee3b270e2678768d1a4291b82d5
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-10-10 23:46:53 -0400

    add functionality for database

--- website/__init__.py
+++ website/__init__.py
@@ -2,14 +2,14 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "random"
-    app.config["SQLALCHEMY_DATABASE_URL"] = f'sqlite:///{DB_NAME}'
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///database.db'
     db.init_app(app)
 
     from .views import views
@@ -17,4 +17,9 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
+    
     return app
