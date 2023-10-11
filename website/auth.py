c165c41da09871a8406aeb8507bb0599ead8510b
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-10-11 00:11:08 -0400

    added login requirements and successful logins with flask_login

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -18,6 +19,7 @@ def login():
             curr_password = generate_password_hash(password, method="sha256")
             if curr_password == user.password:
                 flash("Logged in", category="success")
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash("Incorrect info", category="error")
@@ -27,8 +29,10 @@ def login():
     return render_template("login.html")
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return ""
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route("/signup", methods=["GET", "POST"])
 def signup():
@@ -54,7 +58,7 @@ def signup():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method="sha256"))
             db.session.add(new_user)
             db.session.commit()
-
+            login_user(user, remember=True)
             flash("Account created!", category="success")
             return redirect(url_for('views.home'))
 
