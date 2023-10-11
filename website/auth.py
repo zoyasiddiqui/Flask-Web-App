03e971ef9a311c28c960bfc2565ab2e172d90290
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-10-10 22:49:15 -0400

    render templates

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route("/login")
 def login():
-    return ""
+    return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
@@ -12,4 +12,4 @@ def logout():
 
 @auth.route("/signup")
 def signup():
-    return ""
+    return render_template("signup.html")
