522da3f6eb6df0c896cf25354793affd1eda68cf
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-10-10 23:04:07 -0400

    add POST requests for login

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
     return ""
 
-@auth.route("/signup")
+@auth.route("/signup", methods=["GET", "POST"])
 def signup():
     return render_template("signup.html")
