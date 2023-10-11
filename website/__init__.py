12173323eb716decaa2030b657e3bf976cfd65e7
Zoya Siddiqui <zoyasdq.04@gmail.com>
2023-10-10 22:17:41 -0400

    created flask application

--- website/__init__.py
+++ website/__init__.py
@@ -1,1 +1,9 @@
 # creating this file turns this website into a package
+
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "random"
+
+    return app
